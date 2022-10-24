47dfbf7789f2116cc1e59c308046c423f3a43835
Beagle4ce <beagle4ce@Beagle4cedeMacBook-Pro.local>
2022-10-24 11:31:16 +0800

    Upload new posts or updated to github.

--- hugohook.sh
+++ hugohook.sh
@@ -3,11 +3,15 @@ echo 'Start hugo hook'
 
 hugo -D
 
+echo '\n\n'
+
 cp -R public/* docs/
 
 git add .
 
 git commit -am "Upload new posts or updated to github."
+
+echo '\n\n'
 
 git push origin main
 
